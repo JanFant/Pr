1c25ea48009aaf296873d83aed003ca048bc5987
JanFan <termal12345@gmail.com>
2019-03-25 09:40:56 +0600

    обновил fp.bash

--- src-FP/fp.bash
+++ src-FP/fp.bash
@@ -1,12 +1,13 @@
 #!/bin/bash
-        if (md5sum -c /etc/local.d/sumfp 2>/dev/null | grep "FAILED"); then
-        echo "Checksum doesn't match" > /dev/kmsg
-        else
-        (
-        cd /root
-        while true
-        do
-        ./fpcpc > /dev/null
-        done
-        )
-        fi
+#	if (md5sum -c /etc/local.d/sumfp 2>/dev/null | grep "FAILED"); then
+#	echo "Checksum doesn't match" > /dev/kmsg
+#	else
+#	(
+	cd /root
+	while true
+	do
+	rm *.bin
+	./fpcpc > /dev/null
+	done
+#	)
+#	fi
