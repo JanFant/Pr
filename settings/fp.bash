48c729e01530ce3df511615c2a8ff2a4f36145b4
JanFan <termal12345@gmail.com>
2019-05-12 11:27:29 +0600

    добавил fp

---
+++ settings/fp.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+#	if (md5sum -c /etc/local.d/sumfp 2>/dev/null | grep "FAILED"); then
+#	echo "Checksum doesn't match" > /dev/kmsg
+#	else
+#	(
+	rm /var/log/messages
+	cd /root
+	while true
+	do
+	rm *.bin
+	./fpcpc > /dev/null
+	done
+#	)
+#	fi
