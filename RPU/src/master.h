1c8e4ebea3098b7c6cc6f3b9b08a3604010d2979
JanFan <termal12345@gmail.com>
2019-11-09 13:50:39 +0600

    ff

--- RPU/src/master.h
+++ RPU/src/master.h
@@ -1125,9 +1125,11 @@ void InitSetConst(void){	//Инициализация переменных дл�
 }
 
 // Вставка к VCHS
-void init_dataVchs(vchs_data *vch_data) {
+void init_dataVchs(vchs_data *vch_data)
+{
     int i;
-    for (i = 0; i < 2; i++) {
+    for (i = 0; i < 2; i++)
+    {
         vch_data->takt[i] = 0.5;
         vch_data->cykl[i] = 0.5;
         vch_data->cyklS[i] = 0.5;
@@ -1137,41 +1139,57 @@ void init_dataVchs(vchs_data *vch_data) {
     }
 }
 
-void VCHS_post(vchs_data *vch_data) {
+void VCHS_post(vchs_data *vch_data)
+{
     float fslow = 0, ffast = 0, zer = 0.0;
     int i;
-    for (i = 0; i < 2; i++) {
-        if (vch_data->perm[i] <= 0) {
-                if(vch_data->SVCHS[i] == 1){
-                    if( vch_data->cyklS[i] == 10)
-                        vch_data->cyklS[i] = 1.0;
-                    else
-                        vch_data->cyklS[i] = 0.01;
-                    vch_data->SVCHS[i] = 0;
-                    vch_data->takt[i] = 0;                    
-                    vch_data->cykl[i] = vch_data->cyklS[i]; 
-                    continue;
-                }    
-                ffast = vch_data->tempI[i] / vch_data->takt[i];
-                vch_data->fvch[i] = ffast;
+    for (i = 0; i < 2; i++)
+    {
+        if (vch_data->perm[i] <= 0)
+        {
+            if (vch_data->SVCHS[i] == 1)
+            {
+                if (vch_data->cyklS[i] == 10)
+                    vch_data->cyklS[i] = 1.0;
+                else
+                    vch_data->cyklS[i] = 0.01;
+                vch_data->SVCHS[i] = 0;
                 vch_data->takt[i] = 0;
-                if(ffast < 40.0 )
-                    vch_data->cyklS[i] = 10;
-                    else   
-                    vch_data->cyklS[i] = 1 / ((ffast / 64000) + 1);
-
-                vch_data->cykl[i] = vch_data->cyklS[i]; 
+                vch_data->cykl[i] = vch_data->cyklS[i];
+                continue;
+            }
+            ffast = vch_data->tempI[i] / vch_data->takt[i];
+            vch_data->fvch[i] = ffast;
+            vch_data->takt[i] = 0;
+            // if(ffast < 40.0 )
+            //     vch_data->cyklS[i] = 10;
+            if (ffast < 50.0){
+                if (vch_data->tempI[i] <= 1){
+                    vch_data->cyklS[i] = 50;
+                }
+                else{
+                    fslow = 50 / vch_data->tempI[i];
+                    if (fslow <= 10)
+                        vch_data->cyklS[i] = 10;
+                    else
+                        vch_data->cyklS[i] = fslow;
+                }
+            }
+            else{
+                vch_data->cyklS[i] = 1 / ((ffast / 64000) + 1);
+            }
+            vch_data->cykl[i] = vch_data->cyklS[i];
         }
     }
     for (i = 0; i < 2; i++)
-        if (fpcomp(&vch_data->cykl[i], &zer) > 0) {
+        if (fpcomp(&vch_data->cykl[i], &zer) > 0)
+        {
             vch_data->takt[i] += takt;
             vch_data->cykl[i] -= takt;
             vch_data->perm[i] = fpcomp(&vch_data->cykl[i], &zer);
         }
     vch_data->K01VCHS.f = vch_data->fvch[0];
     vch_data->K02VCHS.f = vch_data->fvch[1];
-
 }/* Определение внутренних параметров */
 
 /* Определение констант ПЗУ и ЭСППЗУ*/
